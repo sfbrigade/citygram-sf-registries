2af0b47ef33730cac1d2188599c62a5467f32813
John Mertens <john.mertens@gmail.com>
2014-10-25 22:31:03 -0700

    Add (and prettify) the title to the feature records.

--- connectors/street-use-permits/app.rb
+++ connectors/street-use-permits/app.rb
@@ -2,6 +2,26 @@ require 'faraday'
 require 'sinatra'
 require 'json'
 
+TITLE_TEMPLATE = <<-CFA.gsub(/\s*\n/,' ').chomp(' ')
+A new permit has been issued for %{permit_type}, at %{streetname}
+between %{cross_street_1} and %{cross_street_2}, from %{permit_start_date}
+to %{permit_end_date}.
+CFA
+
+# Poor man's titleize w/o ActiveSupport
+class String
+  def titleize
+    gsub(/\b([A-Za-z])+|\b\d+[A-Za-z]{2}\b/) do |match|
+      "#{match[0].upcase}#{match[1..-1].downcase}"
+    end
+  end
+end
+
+def date_cleanup(date_str)
+  date_str.gsub!(/T[\d\:]+$/,'')
+  Time.parse(date_str).strftime("%b %e, %Y")
+end
+
 get '/street-use-permits' do
   url = URI('http://data.sfgov.org/resource/b6tj-gt35.json')
   url.query = Faraday::Utils.build_query(
@@ -15,28 +35,38 @@ get '/street-use-permits' do
 	  " AND permit_end_date IS NOT NULL"+
 	  " AND approved_date > '#{(DateTime.now - 7).iso8601}'"
   )
+
   connection = Faraday.new(url: url.to_s)
+
+  # Query the data.sfgov.org endpoint
   response = connection.get
+
   collection = JSON.parse(response.body)
-  #
-  #features = collection.map do |record|
-	#  {
-#		  'id' => record['dbi_application_number'],
-#		  'type' => 'Feature',
-#			  'properties' => record.merge(
-#				  'title' => record['frontage'],
-#				  ),
-#			  'geometry' => {
-#			  'type' => 'Point',
-#			  'coordinates' => [
-#				  record['longitude'].to_f,
-#				  record['latitude'].to_f
-#		  ]
-#		  }
-#	  }
-#  end
+
+  features = collection.map do |record|
+    # Apply any transformations needed to the text being sent to our
+    # title "mad lib" above.
+    title_pieces = {
+      :permit_type => record['permit_type'],
+      :streetname => record['streetname'].titleize,
+      :cross_street_1 => record['cross_street_1'].titleize,
+      :cross_street_2 => record['cross_street_2'].titleize,
+      :permit_start_date => date_cleanup(record['permit_start_date']),
+      :permit_end_date => date_cleanup(record['permit_end_date'])
+    }
+
+    # Return the feature as a hash, which we will convert to json.
+    {
+      'id' => record['permit_number'],
+      'type' => 'Feature',
+      'properties' => record.merge('title' => TITLE_TEMPLATE % title_pieces),
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [0.0, 0.0]
+      }
+    }
+  end
 
   content_type :json
-  #JSON.pretty_generate('type' => 'FeatureCollection', 'features' => features)
-  JSON.pretty_generate(collection)
+  JSON.pretty_generate('type' => 'FeatureCollection', 'features' => features)
 end
