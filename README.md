28fe998f8c916d141be2a5588d8e18147dc126fa
Andrew Hyder <hyd415@gmail.com>
2014-10-27 16:20:09 -0700

    Moved both routes to top app.rb

--- README.md
+++ README.md
@@ -3,9 +3,7 @@ citygram-sf-registries
 
 All of the ETL scripts for Citygram SF
 
-Some sample/WiP connectors can be found in ./connectors.  These are
-currently standalone ruby apps that export different data sources
-in GeoJSON format.  For more information and a getting started
+Our two initial datasets are new Tree Plantings and temporary Tow Away Zones. Both ETL routes are described in app.rb. For more information and a getting started
 guide, see http://bit.ly/citygramsf.
 
 ## TODO
--- app.rb
+++ app.rb
@@ -1,5 +1,77 @@
 require 'faraday'
 require 'sinatra'
+require 'json'
 
-get '/san-francisco-street-tree-plantings' do
-    url = URI('https://data.sfgov.org/api/views/tkzw-k3nq/rows.json')
+get '/tree-planting' do
+  url = URI('http://data.sfgov.org/resource/tkzw-k3nq.json')
+  url.query = Faraday::Utils.build_query(
+    '$order' => 'plantdate DESC',
+    '$limit' => 100,
+    '$where' => "treeid IS NOT NULL"+
+    " AND permitnotes IS NOT NULL"+
+    " AND latitude IS NOT NULL"+
+    " AND longitude IS NOT NULL"+
+    " AND plantdate > '#{(DateTime.now - 365).iso8601}'"
+  )
+  connection = Faraday.new(url: url.to_s)
+  response = connection.get
+  collection = JSON.parse(response.body)
+  features = collection.map do |record|
+    {
+      'id' => record['treeid'],
+      'type' => 'Feature',
+      'properties' => record.merge('title' => record['permitnotes']),
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [
+          record['longitude'].to_f,
+          record['latitude'].to_f
+        ]
+      }
+    }
+  end
+
+  content_type :json
+  JSON.pretty_generate('type' => 'FeatureCollection', 'features' => features)
+end
+
+require 'faraday'
+require 'sinatra'
+require 'json'
+
+get '/tow-away-zones' do
+  url = URI('http://data.sfgov.org/resource/cqn5-muyy.json')
+  url.query = Faraday::Utils.build_query(
+	  '$order' => 'updatedat DESC',
+	  '$limit' => 100,
+	  '$where' => "mta_status_code = 'Approved'"+
+	  " AND dbi_application_number IS NOT NULL"+
+	  " AND frontage IS NOT NULL"+
+	  " AND fromaddress IS NOT NULL"+
+	  " AND toaddress IS NOT NULL"+
+	  " AND streetname IS NOT NULL"+
+	  " AND updatedat > '#{(DateTime.now - 7).iso8601}'"
+  )
+  connection = Faraday.new(url: url.to_s)
+  response = connection.get
+  collection = JSON.parse(response.body)
+  features = collection.map do |record|
+	  {
+		  'id' => record['dbi_application_number'],
+		  'type' => 'Feature',
+			  'properties' => record.merge(
+				  'title' => record['frontage'],
+				  ),
+			  'geometry' => {
+			  'type' => 'Point',
+			  'coordinates' => [
+				  record['longitude'].to_f,
+				  record['latitude'].to_f
+		  ]
+		  }
+	  }
+  end
+
+  content_type :json
+  JSON.pretty_generate('type' => 'FeatureCollection', 'features' => features)
+end
